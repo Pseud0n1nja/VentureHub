ba1d98022ad980569fca9cd7b815418fdc78a955
Safak Ozkan <shafaksan@gmail.com>
2017-12-22 14:10:24 -0800

    f1_score=0.755

--- helpers_DL.py
+++ helpers_DL.py
@@ -10,7 +10,7 @@ from matplotlib.ticker import MaxNLocator
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-def return_ROC_statistics(y_pred, y_true, threshold=np.linspace(0,1,26)):
+def ROC_statistics(y_pred, y_true, threshold=np.linspace(0,1,26)):
     recall, f1_score, fpr, accuracy, precision = [], [], [], [], []
     
     if not hasattr(threshold, '__iter__'):
@@ -78,31 +78,28 @@ def print_runtime(start, p_flag=True):
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-def plotter(mae_train_arr, mae_cv_arr, mae_test_arr, loss_arr, BATCH_SIZE):
+def plotter(loss_cv_arr, loss_train_arr, batch):
     fig = plt.figure()
-    ax1 = fig.add_subplot(121)
-    ax2 = fig.add_subplot(122)
+    ax = fig.add_subplot(111)
 
-    n_epoch = len(mae_train_arr)
-    ax1.plot(range(1, n_epoch+1), mae_test_arr, 'bd-', alpha=.6);
-    ax1.plot(range(1, n_epoch+1), mae_cv_arr, 'rd-', alpha=.6);
-    ax1.plot(range(1, n_epoch+1), mae_train_arr, 'kd-', alpha=.6);
-    ax1.set_xlim(left=1);
-    #ax1.set_ylim(0.5, 0.65)
-    i_min = np.argmin(mae_cv_arr)
-    ax1.set_title('MAE_cv: {:6.4f}, BATCH_SIZE: {}*256'.format(mae_cv_arr[i_min], BATCH_SIZE//256))
-    ax1.legend(['test', 'cv', 'train'])
-    ax2.plot(range(1, n_epoch+1), loss_arr, 'kd-', alpha=.6);
+    n_epoch = len(loss_cv_arr)
+    ax.plot(range(1, n_epoch+1), loss_cv_arr, 'bd-', alpha=.6);
+    ax.plot(range(1, n_epoch+1), loss_train_arr, 'kd-', alpha=.6);
     
-    ax2.set_title('Training Loss (RMS)')
-    ax1.set_xlabel('epochs')
-    ax2.set_xlabel('epochs')
-    #ax2.set_ylim(0.7, 1)
-    i_min = np.argmin(mae_cv_arr)
-    ax1.plot(i_min+1,mae_cv_arr[i_min], 'ro', markersize=13,
-                markeredgewidth=2, markerfacecolor='None')
+    i_min = np.argmin(loss_cv_arr)
+    ax.set_title('loss_cv: {:6.4f}, BATCH_SIZE: {}*256'.format(loss_cv_arr[i_min], batch.BATCH_SIZE//256))
+    ax.set_xlim(left=1)
+    ax.set_xlabel('epochs')
+    ax.plot(i_min+1, loss_cv_arr[i_min], 'b.', markersize=14,
+                markeredgewidth=4, alpha=.75)
     
-    return ax1, ax2
+    i_min_f1 = batch.ckpt_epoch-1
+    ax.plot(i_min_f1+1, loss_cv_arr[i_min_f1], 'ro', markersize=15,
+                markeredgewidth=5, markerfacecolor='None', alpha=.75)
+
+    ax.legend(['cv', 'train', 'argmin cv', 'argmin f1_score'])
+    
+    return ax
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
@@ -147,6 +144,7 @@ class Batch(object):
         self.BATCH_SIZE = BATCH_SIZE
         self.i0 = np.inf
         self.i1 = np.inf
+        self.batch_no = 0
         self.epoch = 0
         self.broken = False
         self.last_batch = False
@@ -157,62 +155,69 @@ class Batch(object):
         if self.i1 >= len(self.R):
             # new epoch.
             self.epoch += 1
+            self.batch_no = 1
             self.broken = False
             self.last_batch = False
+            
             # reset the counter. 
             self.i0 = 0
             self.i1 = self.i0 + self.BATCH_SIZE
-            #print('Epoch %d %s' % (self.epoch, '_'*73))
         else:
+            self.batch_no += 1
             self.i0 = self.i0 + self.BATCH_SIZE
             self.i1 = min(self.i0 + self.BATCH_SIZE, len(self.R))
             if self.i1 == len(self.R):
                 self.last_batch = True
             if self.i1 - self.i0 < self.BATCH_SIZE:
                 self.broken = True
-                return None, None
+                # broken_batch_size
+                bbs = self.i1-self.i0
+                X = np.zeros((bbs, self.n_input))
+                X[np.arange(bbs), self.R_indices[self.i0:self.i1, 0]] = 1
+                X[np.arange(bbs), self.n_investors + self.R_indices[self.i0:self.i1, 1]] = 1
+                y = self.R[self.i0:self.i1].reshape(bbs,1)
+                return X, y
             
-
+        # Create a zeros input vector
         X = np.zeros((self.BATCH_SIZE, self.n_input))
+        # set the VC firm index to 1
         X[np.arange(self.BATCH_SIZE), self.R_indices[self.i0:self.i1, 0]] = 1
+        # set the startup index to 1
         X[np.arange(self.BATCH_SIZE), self.n_investors + self.R_indices[self.i0:self.i1, 1]] = 1
         y = self.R[self.i0:self.i1].reshape(self.BATCH_SIZE,1)
 
         # add number of affiliations between investor and startup
-        X[np.arange(self.BATCH_SIZE), -1*np.ones(self.BATCH_SIZE).astype(np.int64)] = 0
+        X[np.arange(self.BATCH_SIZE), (-1)*np.ones(self.BATCH_SIZE).astype(np.int64)] = 0
         
         return X, y
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-def evaluate_preds_and_mae(sess, cv_R, cv_R_indices, n_investors, n_input, y_pred, X, y, BATCH_SIZE):     
+def evaluate_preds_and_loss(sess, cv_R, cv_R_indices, loss_op, y_pred_op, X, y, BATCH_SIZE, n_investors, n_input):     
+    preds = np.zeros(len(cv_R))
+    epoch_loss_cv = 0
     batch = Batch(cv_R_indices, cv_R, n_investors, n_input, BATCH_SIZE=BATCH_SIZE) 
-    preds = np.zeros((len(cv_R) // BATCH_SIZE) * BATCH_SIZE)
-    i0 = (-1) * BATCH_SIZE
-    i1 = 0
-    for step in range(len(cv_R) // BATCH_SIZE):
+
+    while not batch.last_batch == True:
         batch_X, batch_y = batch.next()
-        i0 = i0 + BATCH_SIZE
-        i1 = i0 + BATCH_SIZE
-        preds[i0:i1] = sess.run(y_pred, feed_dict={X: batch_X, y: batch_y})[:,0]
+        out = sess.run([loss_op, y_pred_op], feed_dict={X: batch_X, y: batch_y})
+        batch_loss_cv, preds[batch.i0:batch.i1] = out[0], out[1][:,0]
+        epoch_loss_cv += batch_loss_cv*(batch.i1-batch.i0)
         
-    mae = np.mean(np.abs(cv_R[:i1] - preds))
-    
-    return preds, mae
+    return preds, epoch_loss_cv/len(cv_R)
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 def construct_graph(LAMBDA=0, lr=0.001, BATCH_SIZE=256, n_input=106603):
     
-    X = tf.placeholder(dtype=tf.float32, shape=(BATCH_SIZE, n_input))
-    y = tf.placeholder(dtype=tf.float32, shape=(BATCH_SIZE, 1))
+    X = tf.placeholder(dtype=tf.float32, shape=(None, n_input))
+    y = tf.placeholder(dtype=tf.float32, shape=(None, 1))
     
-    # Define layer weights as tf.Variable
     n_hidden_1, n_hidden_2 = 100, 100 
     
-    
+    # Declare layer weights as tf.Variable
     weights = {
         'h1': tf.Variable(tf.truncated_normal(shape=(n_input, n_hidden_1), mean=np.sqrt(0), stddev=0.2), dtype=tf.float32),
         'h2': tf.Variable(tf.truncated_normal(shape=(n_hidden_1, n_hidden_2), mean=np.sqrt(0), stddev=0.2), dtype=tf.float32),
@@ -223,19 +228,19 @@ def construct_graph(LAMBDA=0, lr=0.001, BATCH_SIZE=256, n_input=106603):
         'b2': tf.Variable(tf.truncated_normal(shape=(n_hidden_2,), mean=np.sqrt(0), stddev=0.2), dtype=tf.float32),
         'out': tf.Variable(tf.truncated_normal(shape=(1,), mean=np.sqrt(0), stddev=0.2), dtype=tf.float32) }
     # ..............................................................................................................
-    # Add Hidden Layer 1
+    # Hidden Layer 1
     layer_1 = tf.add(tf.matmul(X, weights['h1']), biases['b1'])
     layer_1 = tf.tanh(layer_1)
 
-    # Add Hidden Layer n
+    # Hidden Layer n
     layer_n = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
     layer_n = tf.tanh(layer_n)
 
     # Output Layer
-    logits = tf.matmul(layer_n, weights['out']) + biases['out']    
-    y_pred = tf.sigmoid(logits)
+    logit = tf.matmul(layer_n, weights['out']) + biases['out']    
+    y_pred = tf.sigmoid(logit)
     # Define loss and optimizer
-    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=y))
+    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logit, labels=y))
     optimizer = tf.train.AdamOptimizer(learning_rate=lr)
     train = optimizer.minimize(loss)
 
@@ -246,23 +251,25 @@ def construct_graph(LAMBDA=0, lr=0.001, BATCH_SIZE=256, n_input=106603):
 
 def train_the_model(train_R_indices, train_R, cv_R_indices, cv_R, test_R_indices, test_R,
                BATCH_SIZE, NUM_EPOCHS, LAMBDA, lr, 
-               train, loss, X, y, y_pred,
-               n_investors, n_input):
+               train_op, loss_op, y_pred_op, X, y, 
+               n_investors, n_input, threshold):
     
     start = time.time()
     n_batches = len(train_R) // BATCH_SIZE
     init = tf.global_variables_initializer()
     batch = Batch(train_R_indices, train_R, n_investors, n_input, BATCH_SIZE=BATCH_SIZE)
-    _loss, _reg, batch_no = 0, 0, 0
-    mae_train_arr, mae_cv_arr , mae_test_arr, loss_arr = [], [], [], []
+    epoch_loss_train, _reg = 0, 0
+    loss_cv_arr , loss_train_arr = [], []
     
     best_save_score = -np.inf
     
-    print('NUM_EPOCHS: {}\nLAMBDA: {}\nlr: {}\nn_batches: {}\nBATCH_SIZE: {}'.format(\
-                                            NUM_EPOCHS, LAMBDA, lr, n_batches, BATCH_SIZE))
+    print('NUM_EPOCHS: {}\nLAMBDA: {}\nlr: {}\nn_batches: {}\nBATCH_SIZE: {}\nthreshold: {}'.format(\
+                                            NUM_EPOCHS, LAMBDA, lr, n_batches, BATCH_SIZE, threshold))
     
     # Add ops to save and restore all the variables.
     saver = tf.train.Saver()
+
+    print('start SGD iterations...', end='\r')
     
     with tf.Session() as sess:
         sess.run(init)
@@ -270,59 +277,63 @@ def train_the_model(train_R_indices, train_R, cv_R_indices, cv_R, test_R_indices
         while not (batch.epoch == NUM_EPOCHS and batch.last_batch == True):
             batch_X, batch_y = batch.next()
             if batch.i0 == 0:
-                print('Epoch %d %s' % (batch.epoch, '_'*73))
+                print('Epoch %d %s' % (batch.epoch, '_'*62))
                 
-            if not batch.broken:
-                batch_no += 1
-                _, _batch_loss = sess.run([train, loss], feed_dict={X: batch_X, y: batch_y})
-                _loss += _batch_loss
-                
-                print("batch_no: {}, _loss estimate: {:6.4f}, t={:6.1f} sec".format(
-                        batch_no, _loss/batch_no, time.time()-epoch_end), end='\r') 
+            _, _batch_loss_train = sess.run([train_op, loss_op], feed_dict={X: batch_X, y: batch_y})
+
+            epoch_loss_train += _batch_loss_train*(batch.i1-batch.i0)
+            
+            print("batch_no:{}/{}, loss_train:{:6.4f}, t={:0.1f} sec".format(
+                                                    batch.batch_no, n_batches, epoch_loss_train/batch.i1,
+                                                    time.time()-epoch_end), end='\r') 
             
             if batch.last_batch:  
-                # fetch the mae's
-                print('\nEvaluating MAE on training set...', end='\r')
-                ev_start = time.time()
-                preds_train, _mae_train = evaluate_preds_and_mae(
-                                    sess, train_R, train_R_indices, n_investors, n_input, y_pred, X, y, BATCH_SIZE)
-                print('Evaluating MAE on cv set...      ', end='\r')
-                preds_cv, _mae_cv = evaluate_preds_and_mae(
-                                    sess, cv_R, cv_R_indices, n_investors, n_input, y_pred, X, y, BATCH_SIZE)
-                print('Evaluating MAE on test set...    ', end='\r')
-                preds_test, _mae_test = evaluate_preds_and_mae(
-                                    sess, test_R, test_R_indices, n_investors, n_input, y_pred, X, y, BATCH_SIZE)
+                # collect some statistics for printing the loss function etc
+                # fetch the losses
                 
-                # threshold=0.7 <== an important parameter !!!
-                _, _, _precision_cv, _recall_cv, _f1_score_cv, _ = return_ROC_statistics(preds_cv, cv_R, threshold=.7)
+                print('\nEvaluating loss_cv and preds_cv on cv set... ', end='\r')
+                preds_cv, _loss_cv = evaluate_preds_and_loss(
+                                          sess, cv_R, cv_R_indices, loss_op, y_pred_op, X, y, BATCH_SIZE, n_investors, n_input)
                 
-                mae_train_arr.append(_mae_train)
-                mae_cv_arr.append(_mae_cv)
-                mae_test_arr.append(_mae_test)
-                loss_arr.append(_loss/n_batches)
-                mean_preds = np.mean(preds_cv)
+                # threshold ~ 0.7 <== an important parameter !!!
+                print('Calculating ROC curve, threshold: {:3.1f}         '.format(threshold), end='\r')
+                #_, _, _precision_cv, _recall_cv, _f1_score_cv, _ = ROC_statistics(preds_cv, cv_R, threshold=threshold)
                 
-                # printing....
-                print('\nmae_train: %6.4f, **mae_cv: %6.4f**, mae_test: %6.4f,  mean(preds_cv): %6.4f, ' % \
-                      (_mae_train, _mae_cv, _mae_test, np.mean(preds_cv)), end = '')
+                _, _, _, _, f1_score_cv, _ = ROC_statistics(preds_cv, cv_R)
+                # f1_score_cv is an array.
+                idx = np.nanargmax(f1_score_cv)
+                _f1_score_cv = f1_score_cv[idx]
+                # retrieve the threshold value where f1_score_cv reaches a maximum.
+                threshold = np.linspace(0,1,len(f1_score_cv))[idx]
                 
+                loss_cv_arr.append(_loss_cv)
+                loss_train_arr.append(epoch_loss_train/(batch.i1))
+                                
                 # resetting some iteration variables....
-                _loss_S = _loss
-                batch_no, _loss, _reg = 0, 0, 0
-                
+                epoch_loss_train, _reg = 0, 0
                 epoch_end = time.time()
-                
-                # Save model if recall_cv has reached a minimum.
+
+                # Save model if _f1_score_cv has reached a minimum.
                 if (_f1_score_cv > best_save_score):
                     best_save_score = _f1_score_cv
-                    save_path = saver.save(sess, "saved_models/best_model.ckpt")
-                    print(',  CHECKPOINT!!', end='')
+                    save_path = saver.save(sess, 'saved_models/DL_models/best_model.ckpt')
+                    ckpt = '  !! CHECKPOINT!!'
+                    batch.ckpt_epoch = batch.epoch
+                else: 
+                    ckpt = ''
+                # printing....
+                print('loss_train: {0:6.4f}, **loss_cv: {1:6.4f}**, f1_score_cv: {2:6.4f} @threshold:{3:1.2f} {4:3s}'.format(
+                                                         loss_train_arr[-1], loss_cv_arr[-1], _f1_score_cv, threshold, ckpt))
                 
-                print(', f1_score: {:6.4f}'.format(_f1_score_cv))
+                with open('saved_models/loss_train_and_cv.pkl','wb') as f:
+                    pickle.dump((loss_train_arr, loss_cv_arr, batch), f)
                 
     print()
-    return mae_train_arr, mae_cv_arr, mae_test_arr, loss_arr
-    
-    
-    
-    
+    return loss_cv_arr, loss_train_arr, batch
+
+
+
+
+
+
+
